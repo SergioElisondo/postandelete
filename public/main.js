4b50cd2985c2daeb8528c63d3375b58e6fa38347
Sergio Elisondo <sergioelisondo@gmail.com>
2021-04-04 13:22:53 -0700

    adding the public folder

---
+++ public/main.js
@@ -0,0 +1,16 @@
+// STEP 26 START
+const update = document.querySelector('#update-button')
+
+update.addEventListener('click', _ => {
+  fetch('/quotes', {
+    method: 'put',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      name: 'Darth Vadar',
+      quote: 'I find your lack of faith disturbing.'
+    })
+  })
+})
+// STEP 26 END
+
+
